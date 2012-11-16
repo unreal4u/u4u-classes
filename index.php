7a1d612714ae17e5aa2685220ff29d64fdaf8e8b
Camilo Sperberg <me@unreal4u.com>
2012-11-16 23:16:24 +0100

    Facilitating work

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<?php echo 'Index of classes';
+<?php
+
+include('stable-versions.php');
+
+$definedConstants = get_defined_constants(true);
+foreach($definedConstants['user'] AS $definedConstantKey => $definedConstantValue) {
+    echo '<a href="./'.dirname($definedConstantValue).'/">'.strtolower($definedConstantKey).'</a><br />';
+}
---
+++ stable-versions.php
@@ -0,0 +1,12 @@
+<?php
+
+define('AUTOMATIC_DB_UPDATER', 'Automatic-DB-Updater_v0.1/db_updater.class.php');
+define('CACHE_MANAGER', 'cacheManager_v1.1/cacheManager.class.php');
+define('CSSTACKER', 'CSStacker_v1.3/css.class.php');
+define('DB_MYSQLI', 'db-mysqli_v4.0.0/db_mysqli.class.php');
+define('EXTENDED_PGSQL', 'extended-pgsql_v1.1.0/extended_pgsql.class.php');
+define('HTMLUTILS', 'HTMLUtils_v1.0/html_utils.class.php');
+define('MESSAGESTACK', 'messageStack_v1.0.2/messageStack.class.php');
+define('MYSQL_PAGINATOR', 'mysql-paginator_v1.1/paginator.class.php');
+define('PID', 'pid_v1.2/pid.class.php');
+define('RUT', 'rut_v1.0/rut.class.php');
