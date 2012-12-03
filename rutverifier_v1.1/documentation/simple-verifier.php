fbe52a2276d57693a2dac3df6fed0975ffddb321
Camilo Sperberg <me@unreal4u.com>
2012-12-03 12:34:10 +0100

    Good documentation for rutVerifier

---
+++ rutverifier_v1.1/documentation/simple-verifier.php
@@ -0,0 +1,10 @@
+<?php
+
+include('../rutverifier.class.php');
+
+$rutVerifier = new rutverifier();
+
+var_dump($rutVerifier->isValidRUT('14609732-4'));
+var_dump($rutVerifier->isValidRUT('14.609.732-4'));
+var_dump($rutVerifier->isValidRUT('146097324'));
+var_dump($rutVerifier->isValidRUT('14609732-2'));
