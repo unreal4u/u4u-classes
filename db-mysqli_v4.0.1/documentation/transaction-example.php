59beadc55800c6d63e0c83196452cb0e6bcbe58a
Camilo Sperberg <me@unreal4u.com>
2013-01-15 00:53:57 +0100

    Better time control in examples

--- db-mysqli_v4.0.1/documentation/transaction-example.php
+++ db-mysqli_v4.0.1/documentation/transaction-example.php
@@ -9,13 +9,15 @@
 include('../config.php');
 include('../db_mysqli.class.php');
 
-include('../../functions.php');
+include('../../stable-versions.php');
+$u4uLoader = new u4u_autoloader();
 
-$bench = new benchmarks('databaseException');
+$bench = new benchmark('databaseCalls');
 $db = new db_mysqli();
 
 echo '<pre>';
 
+$bench->beginCounter('newTable');
 try {
     $db->begin_transaction();
     $db->query('DROP TABLE IF EXISTS t');
@@ -26,10 +28,11 @@ try {
 } catch (queryException $e) {
     print_r($e->getMessage().'<br />');
 }
+$bench->endCounter('newTable');
 print_r('<em>First transaction ended, starting with second (which is the one to fail)</em><br />');
 
+$bench->beginCounter('databaseException');
 try {
-    #debug($db);
     $db->begin_transaction();
     $db->query('INSERT INTO t VALUES (?)',3);
     $db->query('INSERT INTO t VALUES (?)',4);
@@ -39,18 +42,25 @@ try {
 } catch (queryException $e) {
     print_r('Transaction failed! The message delivered by the database is: '.$e->getMessage().'<br />');
 }
+$bench->endCounter('databaseException');
 print_r('<em>Second transaction ended, error check:</em><br />');
 
 print_r($db->dbErrors);
 
 print('<br /><em>Database version:</em><br />');
+$bench->beginCounter('databaseVersion');
 try {
     echo $db->version();
     print('<br />');
 } catch (Exception $e) {
     print_r($e->getMessage().'<br />');
 }
+$bench->endCounter('databaseVersion');
+$bench->endCounter('databaseCalls');
 
 print_r('End of execution<br />');
-print('Total time: '.$bench->endCounter('databaseException'));
+print('Total time: '.$bench->getDiff('databaseCalls').'<br />');
+print('New table creation: '.$bench->getDiff('newTable').'<br />');
+print('Exception time: '.$bench->getDiff('databaseException').'<br />');
+print('Version time: '.$bench->getDiff('databaseVersion').'<br />');
 echo '</pre>';
