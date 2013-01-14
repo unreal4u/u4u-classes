edfd0283f954e38dfb0415701a54c9bf97839038
Camilo Sperberg <me@unreal4u.com>
2013-01-15 00:14:55 +0100

    Finished first version of benchmark class
    - Documentation completed
    - Better examples
    - Class can now deliver also peak and normal memory usage at will
    -

--- benchmark_v0.1/benchmark.class.php
+++ benchmark_v0.1/benchmark.class.php
@@ -39,9 +39,64 @@ class benchmark {
 
     /**
      * Returns the exact time
+     *
+     * @return float Returns the exact time+microtime
      */
-    private function getExactTime() {
+    public function getExactTime() {
         return microtime(true);
+    }
+
+    /**
+     * Gets the current used memory footprint
+     *
+     * @param string $format Choose between "B" (bytes), "KB", "KiB", "MB", "MiB", "GB", "GiB". Defaults to "B"
+     * @return int Returns the memory usage in the requested format
+     */
+    public function getMemoryUsage($format='B') {
+        return $this->formatNumber(memory_get_usage(), $format);
+    }
+
+    /**
+     * Gets the peak memory usage
+     *
+     * @param string $format Choose between "B" (bytes), "KB", "KiB", "MB", "MiB", "GB", "GiB". Defaults to "B"
+     * @return int Returns the peak memory usage in the requested format
+     */
+    public function getPeakMemoryUsage($format='B') {
+        return $this->formatNumber(memory_get_peak_usage(), $format);
+    }
+
+    /**
+     * Formats a number according to the given format
+     *
+     * @param float $number Any number
+     * @param string $format Choose between "B" (bytes), "KB", "KiB", "MB", "MiB", "GB", "GiB". Defaults to "B"
+     * @return int Returns the value in the requested format
+     */
+    private function formatNumber($number, $format='B') {
+        $multiplier = 1;
+        switch($format) {
+            case 'KB':
+                $multiplier = 1000;
+                break;
+            case 'KiB':
+                $multiplier = 1024;
+                break;
+            case 'MB':
+                $multiplier = 1000 * 1000;
+                break;
+            case 'MiB':
+                $multiplier = 1024 * 1024;
+                break;
+            case 'GB':
+                $multiplier = 1000 * 1000 * 1000;
+                break;
+            case 'GiB':
+                $multiplier = 1024 * 1024 * 1024;
+                break;
+        }
+
+        return round($number / $multiplier);
     }
 
     /**
@@ -55,8 +110,8 @@ class benchmark {
         $time = $this->getExactTime();
         if (!empty($identifier)) {
             $this->data[$identifier]['startTime'] = $time;
-            $this->data[$identifier]['startMemorySize'] = memory_get_usage();
-            $this->data[$identifier]['startMemoryPeakSize'] = memory_get_peak_usage();
+            $this->data[$identifier]['startMemorySize'] = $this->getMemoryUsage();
+            $this->data[$identifier]['startMemoryPeakSize'] = $this->getPeakMemoryUsage();
         }
 
         return true;
@@ -71,36 +126,36 @@ class benchmark {
     public function endCounter($identifier) {
         // First step: get the current exact time
         $time = $this->getExactTime();
-        $totalTime = 0;
 
         if (!empty($this->data[$identifier]['endTime'])) {
             $totalTime = $this->data[$identifier]['endTime'] - $this->data[$identifier]['startTime'];
         } else if (array_key_exists($identifier, $this->data)) {
             $this->data[$identifier]['endTime'] = $time;
-            $this->data[$identifier]['endMemorySize'] = memory_get_usage();
-            $this->data[$identifier]['endMemoryPeakSize'] = memory_get_peak_usage();
-            $totalTime = $this->data[$identifier]['endTime'] - $this->data[$identifier]['startTime'];
+            $this->data[$identifier]['endMemorySize'] = $this->getMemoryUsage();
+            $this->data[$identifier]['endMemoryPeakSize'] = $this->getPeakMemoryUsage();
         }
 
-        $format = '%.'.$this->decimals.'f';
-        return sprintf($format, $totalTime);
+        return $this->getDiff($identifier);
     }
 
     /**
      * Delivers the memory difference of a identifier
      *
      * @param string $identifier The identifier of the data we want to return
-     * @param string $type Can be "normal" or "peak"
+     * @param string $type Can be "time", "memory" or "peakmemory". Defaults to "time"
      */
-    public function memoryDiff($identifier, $type='normal') {
+    public function getDiff($identifier, $type='time') {
         $return = false;
 
         if (!empty($identifier) AND !empty($this->data[$identifier]['endMemorySize'])) {
             switch($type) {
-                case 'normal':
+                case 'time':
+                    $return = sprintf('%.'.$this->decimals.'f', $this->data[$identifier]['endTime'] - $this->data[$identifier]['startTime']);
+                    break;
+                case 'memory':
                     $return = $this->data[$identifier]['endMemorySize'] - $this->data[$identifier]['startMemorySize'];
                 break;
-                case 'peak':
+                case 'peakmemory':
                     $return = $this->data[$identifier]['endMemoryPeakSize'] - $this->data[$identifier]['startMemoryPeakSize'];
                 break;
             }
--- benchmark_v0.1/documentation/simpleExample.php
+++ benchmark_v0.1/documentation/simpleExample.php
@@ -2,17 +2,21 @@
 
 include('../benchmark.class.php');
 
+function myProcess() {
+    for ($i = 0; $i < 200000; $i++) {
+        $j = round($i * 44 + 33 * $i - $i * sqrt(10), 6);
+    }
+
+    return true;
+}
+
 $benchmark = new benchmark();
 $benchmark->decimals = 10;
 
 $benchmark->beginCounter('theProcess');
-
-for($i = 0; $i < 200000; $i++) {
-    // Random calculation to kill some time
-    $j = round($i * 44 + 33 * $i - $i * sqrt(10), 6);
-}
-
+myProcess();
 $endTime = $benchmark->endCounter('theProcess');
+
 printf('Total time: %.'.$benchmark->decimals.'f<br />', $endTime);
-printf('Total memory: %d<br />', $benchmark->memoryDiff('theProcess'));
-printf('Total memory (Peak): %d<br />', $benchmark->memoryDiff('theProcess', 'peak'));
+printf('Total memory: %d<br />', $benchmark->getDiff('theProcess', 'memory'));
+printf('Total memory (Peak): %d<br />', $benchmark->getDiff('theProcess', 'peakmemory'));
