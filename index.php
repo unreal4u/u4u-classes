f93ef12c9ea34150f25b0650182fe6612f0599cc
Camilo Sperberg <me@unreal4u.com>
2013-05-27 17:30:20 +0200

    If APC is not installed, don't fill up with warnings

--- index.php
+++ index.php
@@ -1,22 +1,31 @@
 <?php
 
 include('stable-versions.php');
-$autoLoader = new u4u\autoLoader();
 
+$output = false;
+$autoLoader = new \u4u\autoLoader();
 $definedConstants = get_defined_constants(true);
 
-// Either way is valid, I prefer second method as it is simpler
-#$cacheManager = $autoLoader->instantiateClass('cacheManager', array('apc'));
-$cacheManager = new u4u\cacheManager('apc');
+try {
+    // Either way is valid, I prefer second method as it is simpler
+    #$cacheManager = $autoLoader->instantiateClass('cacheManager', array('apc'));
+    $cacheManager = new u4u\cacheManager('apc');
+    $output = $cacheManager->load('definedU4UClasses', $definedConstants['user']);
+} catch (Exception $e) {
+    print('APC not installed? '.$e->getMessage());
+}
 
-$output = $cacheManager->load('definedU4UClasses', $definedConstants['user']);
 if ($output === false) {
     $output = '';
     foreach($definedConstants['user'] AS $definedConstantKey => $definedConstantValue) {
         $output .= '<p>Go to class: <a href="./'.dirname($definedConstantValue).'/">'.str_replace('u4u\\', '', strtolower($definedConstantKey)).'</a></p>';
     }
 
-    $cacheManager->save($output, 'definedU4UClasses', $definedConstants['user'], 3600);
+    if (!empty($cacheManager)) {
+        // Silently fail
+        $cacheManager->save($output, 'definedU4UClasses', $definedConstants['user'], 3600);
+    }
+
 }
 
 echo $output;
